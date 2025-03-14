2c85c603c47ada89c8f06cd61d9805da6b299944
MainHieu <hieultbt.2801@gmail.com>
2025-03-14 14:51:30 +0700

    Add js/script.js 1741938688657

---
+++ js/script.js
@@ -0,0 +1,2 @@
+// This file is intentionally left empty as we moved the JavaScript code directly into game.html
+// for simplicity. In a production environment, this would contain the separated JavaScript logic.
